be39e7c9fef488e9d948ce17de12d9c63012fa6f
Kerri Miller <kerri@vholdr.com>
2011-11-19 11:27:43 -0800

    first commit -- hooray!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FizzBuzzWebsite::Application.config.secret_token = '0f67469d78b547cc39e2ab893b246f58a37304fc25c0998156e3e81975967cf19f82ea6a1db83ac98ad88d399341661a562e37f3f827b5df124e3987ab6f528d'
